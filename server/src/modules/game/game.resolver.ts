355ee7f43ba8dbd603197af0fc53ec6b6c0d2454
Alexey Malyshev <lordniken@mail.ru>
2021-07-17 12:15:26 +0300

    Removed unused code

--- server/src/modules/game/game.resolver.ts
+++ server/src/modules/game/game.resolver.ts
@@ -11,7 +11,6 @@ import { GameCreateDto, GameVotingDto } from 'src/dto';
 import { Game, GameInfo, User } from 'src/models';
 import { GameService } from './game.service';
 import { AuthGuard } from '../auth/auth.guard';
-import { StorieService } from '../storie/storie.service';
 import { PubSubEngine } from 'graphql-subscriptions';
 import { events } from 'src/enums';
 import { GameStatus } from 'src/models/GameStatus';
@@ -23,7 +22,6 @@ export class GameResolver {
   constructor(
     private readonly gameService: GameService,
     private readonly userService: UserService,
-    private readonly storieService: StorieService,
     @Inject('PUB_SUB') private pubSub: PubSubEngine,
   ) {}
 
